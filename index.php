03b9c41f093e321adc30da2f1d0ba4190ed3a661
AlfredBrayden <alfredbrayden11@gmail.com>
2025-01-29 21:36:21 +0500

    team section updated

--- index.php
+++ index.php
@@ -214,12 +214,7 @@
               <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
               <h6 class="subtitle">Property Specialist</h6>
               <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
+
             </div>
           </div>
         </div>
@@ -238,12 +233,7 @@
               <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
               <h6 class="subtitle">Property Specialist</h6>
               <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
+
             </div>
           </div>
         </div>
@@ -262,12 +252,7 @@
               <h5 class="mt-4 font-weight-medium mb-0">Michael Doe</h5>
               <h6 class="subtitle">Property Specialist</h6>
               <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-facebook"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="icon-social-behance"></i></a></li>
-              </ul>
+
             </div>
           </div>
         </div>
